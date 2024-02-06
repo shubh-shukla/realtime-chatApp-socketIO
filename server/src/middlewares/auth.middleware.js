c1f931df01ae77767b44b3c76a60a2adbb65755c
Shubham Shukla <shukla111shubh@gmail.com>
2024-02-06 18:48:30 +0530

    added auth middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/APIError.js";
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
