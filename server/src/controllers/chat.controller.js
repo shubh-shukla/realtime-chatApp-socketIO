32e9e5ce74708d0fa998f67c57efb6286909a20d
Shubham Shukla <shukla111shubh@gmail.com>
2024-02-17 21:51:44 +0530

    added chat controllers

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -5,11 +5,25 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { Chat } from "../models/chat.model.js";
 import { User } from "../models/user.model.js";
 
-const createChat = asyncHandler(async (req, res, next) => {
+const FetchChats = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const chats = await Chat.find({
+    members: {
+      $in: [id],
+    },
+  });
+  return res.status(200).json(new ApiResponse(200, chats));
+})
+
+const createGroupChat = asyncHandler(async (req, res, next) => {
   const { members, name } = req.body;
 
   if (!members) {
     return next(new ApiError(400, "Members are required"));
+  }
+
+  if(!name) {
+    return next(new ApiError(400, "Name is required for group chat"));
   }
 
   const validMembers = await Chat.find({
@@ -18,42 +32,50 @@ const createChat = asyncHandler(async (req, res, next) => {
     },
   });
 
-  if (validMembers.length < 2) {
-    return next(new ApiError(400, "At least 2 members are required"));
-  }
-
-  let isGroupChat = false;
-
-  if (validMembers.length > 2) {
-    isGroupChat = true;
-    if(!name) {
-      return next(new ApiError(400, "Name is required for group chat"));
-    }
-  }
-
-  if (validMembers.length === 2) {
-    name =
-      validMembers[0]._id === req.user._id
-        ? validMembers[1].fullName
-        : validMembers[0].fullName;
+  if (validMembers.length < 3) {
+    return next(new ApiError(400, "At least 3 members are required"));
   }
 
   const chat = await Chat.create({
+    chatName : name,
     members : validMembers,
-    name,
-    isGroupChat,
+    isGroupChat : true,
   });
 
-  return res.status(201).json(new ApiResponse(201, chat));
+  return chat
+  // return res.status(201).json(new ApiResponse(201, chat));
 });
 
-const getChat = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const chat = await Chat.findById(id);
+const accessGroupChat = asyncHandler(async (req, res, next) => {
+  const { id } = req.body;
+  let chat = await Chat.findById(id);
   if (!chat) {
-    return next(new ApiError(404, "Chat not found"));
+    chat = createGroupChat(req, res, next);
+  }
+  return res.status(200).json(new ApiResponse(200, chat));
+})
+
+const accessChat = asyncHandler(async (req, res, next) => {
+  const { id } = req.body;
+
+  //find a chat where current user and other user are the only members
+
+  let chat = await Chat.findOne({
+    members: {
+      $all: [req.user._id, id],
+    },
+    isGroupChat : false
+  })
+
+  // let chat = await Chat.findById(id);
+  if (!chat) {
+    const user = await User.findById(id).select("username");
+    chat = await Chat.create({
+      chatName : user.username,
+      members : [req.user._id, id],
+    });
   }
   return res.status(200).json(new ApiResponse(200, chat));
 });
 
-export { createChat, getChat };
+export { createGroupChat, accessChat, FetchChats, accessGroupChat };
