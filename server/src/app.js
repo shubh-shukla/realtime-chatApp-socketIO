f6f282e15f0120da1a57a5ac55eab4f1e8affc6f
Shubham Shukla <shukla111shubh@gmail.com>
2024-03-12 19:41:49 +0530

    added middlewares, routes

---
+++ server/src/app.js
@@ -0,0 +1,36 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+app.use(express.static("public"));
+app.use(cookieParser());
+
+import userRouter from "./routes/user.route.js";
+import chatRouter from "./routes/chat.route.js";
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/chat", chatRouter);
+
+export { app };
